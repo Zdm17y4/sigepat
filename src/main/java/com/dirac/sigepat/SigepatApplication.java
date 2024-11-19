65e80eec2be0d85bf37d9acd8c17e5ae580af490
Mitchel <daronmitchel@gmail.com>
2024-11-19 10:30:29 -0500

    Creacion de la arquitectura con SpringBoot Creacion de la clase Cliente

---
+++ src/main/java/com/dirac/sigepat/SigepatApplication.java
@@ -0,0 +1,13 @@
+package com.dirac.sigepat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigepatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigepatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dirac/sigepat/SigepatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirac.sigepat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigepatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
