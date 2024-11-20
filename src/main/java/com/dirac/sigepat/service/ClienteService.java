4f20ce736b00a383f3c52f38056627c4ed40a0e3
Mitchel <daronmitchel@gmail.com>
2024-11-20 12:37:36 -0500

    Se agregaron metodos a la clase ClienteService, insert, update y delete

--- src/main/java/com/dirac/sigepat/service/ClienteService.java
+++ src/main/java/com/dirac/sigepat/service/ClienteService.java
@@ -7,6 +7,7 @@ package com.dirac.sigepat.service;
 import com.dirac.sigepat.repository.ClienteRepository;
 import com.dirac.sigepat.model.Cliente;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,31 @@ public class ClienteService {
     
     public List<Cliente> getClientes() {
         return clienteRepository.findAll();
+    }
+    
+    /**
+     * El tipo Optional es seguro 
+     * @param id
+     * @return 
+     */
+    
+    // GET
+    public Optional<Cliente> getCliente(Long id) {
+        return clienteRepository.findById(id);
+    }
+    
+    // POST
+    public Cliente insertCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+    
+    // PUT
+    public Cliente updateCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+    
+    // DELETE
+    public void deleteCliente(Long id) {
+        clienteRepository.deleteById(id);
     }
 }
